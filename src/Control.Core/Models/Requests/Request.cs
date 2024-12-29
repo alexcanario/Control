44424d9ccc5576edbafb8a51be5506c2928d8650
Alex Canario <alexcanario@gmail.com>
2024-12-29 23:18:39 +0000

    Added request class

---
+++ src/Control.Core/Models/Requests/Request.cs
@@ -0,0 +1,11 @@
+﻿namespace Control.Core.Models.Requests;
+
+public class Request
+{
+    public string Title { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public int Type { get; set; } 
+    public decimal Amount { get; set; }
+    public long CategoryId { get; set; }
+    public string UserId { get; set; } = string.Empty;
+}
