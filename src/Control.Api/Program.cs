052d0058f9e99864e27a65441f51a42762bd2dd3
Alex Canario <alexcanario@gmail.com>
2024-12-31 11:34:25 +0000

    Request, response andpoint adjustment

--- src/Control.Api/Program.cs
+++ src/Control.Api/Program.cs
@@ -1,6 +1,22 @@
+using Control.Core.Models.Requests;
+using Control.Core.Models.Responses;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapPost(
+    "/v1/categories", 
+    (Request request, Handler handler) => handler.Handle(request))
+    .WithName("Transactions: Create")
+    .WithSummary("Create a new transaction")
+    .Produces<Response>();
 
 app.Run();
+
+public class Handler
+{
+    public Response Handle(Request request)
+    {
+        return new Response { Id = 4, Title =  request.Title };
+    }
+}
