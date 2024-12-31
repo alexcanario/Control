b860fe32bbd34fa49921c13c2698547b814a2e2e
Alex Canario <alexcanario@gmail.com>
2024-12-31 15:42:05 +0000

    feat: added dbContext Fixed AB#60

--- src/Control.Api/Data/AppDbContext.cs
+++ src/Control.Api/Data/AppDbContext.cs
@@ -7,4 +7,5 @@ public class AppDbContextContext : DbContext
 {
     public DbSet<Category> Categories { get; set; } = null!;
     public DbSet<Transaction> Transactions { get; set; } = null!;
+    
 }
