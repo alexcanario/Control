e8b31d5d7c460522131aedd1afae26afd6904ed4
Alex Canario <alexcanario@gmail.com>
2025-04-18 18:27:33 +0100

    feat: added base class

---
+++ src/Dima.Core/Models/Transaction.cs
@@ -0,0 +1,12 @@
+﻿namespace Dima.Core.Models;
+
+public class Transaction : Entity<long>
+{
+    public string Title { get; set; } =  string.Empty;
+    public DateTime PaidOrReceivedAt { get; set; }
+    public ETransactionType Type { get; set; }
+    public decimal Amount { get; set; }
+    public long CategoryId { get; set; }
+    public Category Category { get; set; } =  null!;
+    public string UserId { get; set; } =   string.Empty;
+}
