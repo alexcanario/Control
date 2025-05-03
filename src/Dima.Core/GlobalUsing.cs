fa083d96bcaeb4d4893ae623305ac496d077a4ae
Alex Canario <alexcanario@gmail.com>
2025-05-03 17:04:10 +0100

    Add category management interface and update global usings
    This commit introduces a new `ICategoryHandler` interface in the `Dima.Core.Handlers` namespace, defining methods for creating, updating, deleting, and retrieving categories. Additionally, several global using directives for new namespaces related to models, requests, and responses have been added in `GlobalUsing.cs`, enhancing the overall functionality of the category management system.

--- src/Dima.Core/GlobalUsing.cs
+++ src/Dima.Core/GlobalUsing.cs
@@ -1,4 +1,7 @@
 ﻿global using Dima.Core.Enums;
+global using Dima.Core.Models;
 global using Dima.Core.Models.Abstractions;
+global using Dima.Core.Requests.Categories;
+global using Dima.Core.Responses;
 
 global using System.ComponentModel.DataAnnotations;
---
+++ src/Dima.Core/Handlers/ICategoryHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace Dima.Core.Handlers;
+
+public interface ICategoryHandler
+{
+	Task<Response<Category>> CreateAsync(CreateCategoryRequest request);
+	Task<Response<Category>> UpdateAsync(UpdateCategoryRequest request);
+	Task<Response<Category>> DeleteAsync(DeleteCategoryRequest request);
+	Task<Response<List<Category>>> GetAllAsync(GetAllCategoriesRequest request);
+	Task<Response<Category>> GetByIdAsync(GetCategoryByIdRequest request);
+}
