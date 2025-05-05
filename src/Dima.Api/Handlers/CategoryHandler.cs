442fae82c962c6419b6b430a3e4fc722f8291568
Alex Canario <alexcanario@gmail.com>
2025-05-05 07:39:45 +0100

    Enhance category management functionality
    - Added Microsoft.EntityFrameworkCore namespace to CategoryHandler.cs.
    - Updated CreateAsync to return 201 Created status and improved error handling.
    - Implemented UpdateAsync to retrieve, update, and handle errors for categories.
    - Modified DeleteAsync to remove NotImplementedException.
    - Added Id property to UpdateCategoryRequest for category identification.
    - Retained validation for Title and Description properties in UpdateCategoryRequest.

--- src/Dima.Api/Handlers/CategoryHandler.cs
+++ src/Dima.Api/Handlers/CategoryHandler.cs
@@ -1,5 +1,7 @@
 ﻿using Dima.Api.Data;
 
+using Microsoft.EntityFrameworkCore;
+
 namespace Dima.Api.Handlers;
 
 public class CategoryHandler(AppDbContext Ctx) : ICategoryHandler
@@ -18,23 +20,42 @@ public class CategoryHandler(AppDbContext Ctx) : ICategoryHandler
 			Ctx.Categories.Add(category);
 			await Ctx.SaveChangesAsync();
 
-			return new Response<Category>(category);
+			return new Response<Category>(category, code: StatusCodes.Status201Created);
 		}
 		catch (Exception ex)
 		{
-			return new Response<Category>(category, message:ex.Message);
+			return new Response<Category>(category, message: ex.Message);
 			throw;
 		}
 	}
 
-	public Task<Response<Category>> UpdateAsync(UpdateCategoryRequest request)
+	public async Task<Response<Category>> UpdateAsync(UpdateCategoryRequest request)
 	{
-		throw new NotImplementedException();
+		var category = await Ctx.Categories.Where(c => c.Id == request.Id && c.UserId == request.UserId)
+			.FirstOrDefaultAsync();
+
+		if (category == null)
+			return new Response<Category>(null, code: StatusCodes.Status404NotFound, message: "Categoria não encontrada!");
+
+		try
+		{
+			category.Title = request.Title;
+			category.Description = request.Description;
+
+			Ctx.Categories.Update(category);
+			await Ctx.SaveChangesAsync();
+			return new Response<Category>(category);
+		}
+		catch (Exception e)
+		{
+			// Log the exception (e.g., using Serilog, NLog, etc.)
+			return new Response<Category>(null, code: StatusCodes.Status500InternalServerError,  "Não foi possível alterar a categoria.");
+		}
 	}
 
 	public Task<Response<Category>> DeleteAsync(DeleteCategoryRequest request)
 	{
-		throw new NotImplementedException();
+
 	}
 
 	public Task<Response<IList<Category>>> GetAllAsync(GetAllCategoriesRequest request)
--- src/Dima.Core/Requests/Categories/UpdateCategoryRequest.cs
+++ src/Dima.Core/Requests/Categories/UpdateCategoryRequest.cs
@@ -2,6 +2,8 @@
 
 public class UpdateCategoryRequest : Request
 {
+	public long Id { get; set; }
+
 	[Required(ErrorMessage = "Título inválido!")]
 	[MaxLength(80, ErrorMessage = "Título deve ter no máximo {1} caracteres!")]
 	public string Title { get; set; } = string.Empty;
